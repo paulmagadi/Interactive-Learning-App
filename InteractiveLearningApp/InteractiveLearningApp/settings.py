e3fdbeba831b0f106e571e34ae0cd870950cd017
SQCodes <paulsqmagadi@gmail.com>
2024-07-18 23:43:59 +0300

    modified:   InteractiveLearningApp/InteractiveLearningApp/__pycache__/settings.cpython-312.pyc 	modified:   InteractiveLearningApp/InteractiveLearningApp/__pycache__/urls.cpython-312.pyc 	modified:   InteractiveLearningApp/InteractiveLearningApp/settings.py 	modified:   InteractiveLearningApp/InteractiveLearningApp/urls.py

--- InteractiveLearningApp/InteractiveLearningApp/settings.py
+++ InteractiveLearningApp/InteractiveLearningApp/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -117,6 +118,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
+
+MEDIA_URL = 'media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
