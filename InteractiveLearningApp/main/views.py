a6eca158637899ae8d921f8797ce949408f1f2c8
SQCodes <paulsqmagadi@gmail.com>
2024-07-18 23:34:39 +0300

    new file:   InteractiveLearningApp/main/index.html 	new file:   InteractiveLearningApp/main/templates/main/base.html 	new file:   InteractiveLearningApp/main/urls.py 	modified:   InteractiveLearningApp/main/views.py

--- InteractiveLearningApp/main/views.py
+++ InteractiveLearningApp/main/views.py
@@ -2,14 +2,17 @@
 from django.shortcuts import render
 from .models import Lesson, UserProgress
 
-def lesson_list(request):
-    lessons = Lesson.objects.all()
-    return render(request, 'lessons/lesson_list.html', {'lessons': lessons})
+def home(request):
+    return render(request, 'main/index.html')
 
-def lesson_detail(request, lesson_id):
-    lesson = Lesson.objects.get(id=lesson_id)
-    return render(request, 'lessons/lesson_detail.html', {'lesson': lesson})
+# def lesson_list(request):
+#     lessons = Lesson.objects.all()
+#     return render(request, 'lessons/lesson_list.html', {'lessons': lessons})
 
-def user_progress(request):
-    progress = UserProgress.objects.get(user=request.user)
-    return render(request, 'lessons/user_progress.html', {'progress': progress})
+# def lesson_detail(request, lesson_id):
+#     lesson = Lesson.objects.get(id=lesson_id)
+#     return render(request, 'lessons/lesson_detail.html', {'lesson': lesson})
+
+# def user_progress(request):
+#     progress = UserProgress.objects.get(user=request.user)
+#     return render(request, 'lessons/user_progress.html', {'progress': progress})
