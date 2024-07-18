b6bbafbb69bc2b5818153d0d5223867b211d00f0
SQCodes <paulsqmagadi@gmail.com>
2024-07-18 23:09:36 +0300

    modified:   InteractiveLearningApp/InteractiveLearningApp/__pycache__/settings.cpython-312.pyc 	modified:   InteractiveLearningApp/InteractiveLearningApp/settings.py 	new file:   InteractiveLearningApp/main/__pycache__/__init__.cpython-312.pyc 	new file:   InteractiveLearningApp/main/__pycache__/admin.cpython-312.pyc 	new file:   InteractiveLearningApp/main/__pycache__/apps.cpython-312.pyc 	new file:   InteractiveLearningApp/main/__pycache__/models.cpython-312.pyc 	new file:   InteractiveLearningApp/main/migrations/__pycache__/__init__.cpython-312.pyc

--- InteractiveLearningApp/InteractiveLearningApp/settings.py
+++ InteractiveLearningApp/InteractiveLearningApp/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +122,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
