9a44ebea1f06ef4bd5253208b49caf2e091f284a
SQCodes <paulsqmagadi@gmail.com>
2024-07-20 18:53:21 +0300

    modified:   InteractiveLearningApp/main/__pycache__/views.cpython-311.pyc 	modified:   InteractiveLearningApp/main/templates/main/navbar.html 	modified:   InteractiveLearningApp/main/views.py

--- InteractiveLearningApp/main/views.py
+++ InteractiveLearningApp/main/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from courses.models import Course, Unit, Lesson, Content
 
 def home(request):
-    return render(request, 'main/index.html')
+    courses = Course.objects.all()
+    context = {
+        'courses': courses
+    }
+    return render(request, 'main/index.html', context)
