640fd35b1c35388ac07bbc21f9279229d8aa9ff6
SQCodes <paulsqmagadi@gmail.com>
2024-07-20 23:06:17 +0300

    modified:   InteractiveLearningApp/courses/__pycache__/views.cpython-311.pyc 	modified:   InteractiveLearningApp/courses/views.py 	modified:   InteractiveLearningApp/main/templates/main/navbar.html

--- InteractiveLearningApp/courses/views.py
+++ InteractiveLearningApp/courses/views.py
@@ -2,7 +2,12 @@ from django.shortcuts import render, get_object_or_404
 from .models import Course, Unit, Lesson, Content
 
 def course(request, slug):
+    courses = Course.objects.all()
     course = get_object_or_404(Course, slug=slug)
-    return render(request, 'courses/course.html', {'course': course})
+    context = {
+        'course': course,
+        'courses': courses,
+    }
+    return render(request, 'courses/course.html', context)
 
 
