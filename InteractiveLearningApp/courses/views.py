60a8ac75866dbccca5715804ca2945a772dae782
SQCodes <paulsqmagadi@gmail.com>
2024-07-21 23:31:02 +0300

    modified:   InteractiveLearningApp/courses/__pycache__/views.cpython-311.pyc 	modified:   InteractiveLearningApp/courses/views.py

--- InteractiveLearningApp/courses/views.py
+++ InteractiveLearningApp/courses/views.py
@@ -3,10 +3,18 @@ from .models import Course, Unit, Lesson, Content
 
 def course(request, slug):
     courses = Course.objects.all()
+    units = Unit.objects.all()
+    lessons = Lesson.objects.all()
     course = get_object_or_404(Course, slug=slug)
+    unit = get_object_or_404(Unit, slug=slug)
+    lesson = get_object_or_404(Lesson, slug=slug)
     context = {
         'course': course,
         'courses': courses,
+        'units': units,
+        'unit': unit,
+        'lessons': lessons,
+        'lesson': lesson,
     }
     return render(request, 'courses/course.html', context)
 
