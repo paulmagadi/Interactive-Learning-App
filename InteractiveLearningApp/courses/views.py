c3e25ac372c0b7debf9ceaaee05f93fa7f034402
SQCodes <paulsqmagadi@gmail.com>
2024-07-22 18:28:44 +0300

    modified:   InteractiveLearningApp/courses/__pycache__/views.cpython-311.pyc 	modified:   InteractiveLearningApp/courses/templates/courses/course.html 	modified:   InteractiveLearningApp/courses/templates/courses/sidebar.html 	modified:   InteractiveLearningApp/courses/views.py 	modified:   InteractiveLearningApp/db.sqlite3 	modified:   InteractiveLearningApp/main/__pycache__/views.cpython-311.pyc

--- InteractiveLearningApp/courses/views.py
+++ InteractiveLearningApp/courses/views.py
@@ -3,14 +3,25 @@ from .models import Course, Unit, Lesson, Content
 
 def course(request, slug):
     courses = Course.objects.all()
-    units = Unit.objects.all()
-    lessons = Lesson.objects.all()
-    contents = Content.objects.all()
     course = get_object_or_404(Course, slug=slug)
+    units = Unit.objects.filter(course=course)
+    
+    # Check if a unit is selected via query parameters
+    unit_id = request.GET.get('unit')
+    if unit_id:
+        selected_unit = get_object_or_404(Unit, slug=unit_id)
+        lessons = Lesson.objects.filter(unit=selected_unit)
+        contents = Content.objects.filter(lesson__in=lessons)
+    else:
+        selected_unit = None
+        lessons = Lesson.objects.filter(unit__in=units)
+        contents = Content.objects.filter(lesson__in=lessons)
+
     context = {
-        'course': course,
         'courses': courses,
+        'course': course,
         'units': units,
+        'selected_unit': selected_unit,
         'lessons': lessons,
         'contents': contents,
     }
